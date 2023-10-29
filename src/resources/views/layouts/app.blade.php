2f8f0fe6d8121c0996632661575e283b411c3558
suzuyu0115 <szkyut0115@gmail.com>
2023-10-29 18:00:11 +0900

    Fix: OGPの修正

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -7,11 +7,11 @@
         <meta property="og:description" content="スナック感覚で楽しめる個人開発サービス投稿サイト" />
         <meta property="og:site_name" content="MiniAppHub" />
         <meta property="og:url" content="http://ec2-35-78-211-144.ap-northeast-1.compute.amazonaws.com:8080/" />
-        <meta property="og:image" content="{{ url('images/ogp-image.png') }}" />
+        <meta property="og:image" content="{{ asset('images/ogp-image.png') }}" />
 
         <!-- Twitter-specific tags -->
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:image" content="{{ url('images/ogp-image.png') }}" />
+        <meta name="twitter:image" content="{{ asset('images/ogp-image.png') }}" />
         <meta name="twitter:description" content="スナック感覚で楽しめる個人開発サービス投稿サイト" />
 
         <title>{{ config('app.name', 'Laravel') }}</title>
@@ -19,6 +19,7 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link rel="icon" href="{{ asset('images/favicon.png') }}" type="image/png">
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
